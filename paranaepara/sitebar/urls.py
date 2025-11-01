9e66076ca72870a7d466ad86c21c1732904d986c
CarlosKrupek <carloskrupek@alunos.utfpr.edu.br>
2025-11-01 02:19:33 -0300

    muita coisa
    muita correção pequena pra detalhar, mas agr o cardapio funciona

--- paranaepara/sitebar/urls.py
+++ paranaepara/sitebar/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index,
-    name='index'),
+    path("", views.index, name='index'),
+    path('cardapio/comida/', views.cardapio_comida, name='cardapio_comida'),
+    path('cardapio/tipicas/', views.cardapio_tipica, name='cardapio_tipica'),
+    path('catalogo/jogos/', views.catalogo_jogo, name='catalogo_jogo'),
 ]
